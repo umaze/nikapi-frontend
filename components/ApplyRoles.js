0aeb8082a6133b6719d2e6a134509f67f39be688
Urs Mazenauer <umazenauer@hotmail.com>
2023-03-18 21:32:40 +0100

    show datum on apply roles

--- components/ApplyRoles.js
+++ components/ApplyRoles.js
@@ -2,6 +2,7 @@ import Step from "@/components/Step";
 import {useSelector} from "react-redux";
 import {selectCurrentDemand} from "@/store/activitySlice";
 import SelectAvailability from "@/components/SelectAvailability";
+import styles from "@/styles/ApplyRoles.module.scss";
 
 export default function ApplyRoles({currentStep, stepsSize, register, errors}) {
     const activityDemand = useSelector(selectCurrentDemand);
@@ -11,8 +12,12 @@ export default function ApplyRoles({currentStep, stepsSize, register, errors}) {
         <Step title="Rollen zuweisen" current={currentStep} size={stepsSize}>
             {activityDemand.attributes &&
                 <>
-                    <p><strong>Einsatztyp</strong>: {activityDemand.attributes.einsatztyp.typ}</p>
-                    {!rollen.length && <h3 className="heading-tertiary">Keine Rollen verhanden</h3>}
+                    <div className={styles.infos}>
+                        <p>Datum: <strong>{new Date(activityDemand.attributes.datum).toLocaleDateString('de-CH')}</strong></p>
+                        <p>Einsatztyp: <strong>{activityDemand.attributes.einsatztyp.typ}</strong></p>
+                        {!rollen.length && <h3 className="heading-tertiary">Keine Rollen verhanden</h3>}
+                    </div>
+
                     {rollen.map((r, i) => (
                         <SelectAvailability key={i} role={r} register={register} errors={errors}/>
                     ))}
