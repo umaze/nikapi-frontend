56a2aafae692ee62f217b22f40d4b824cc106bb8
Urs Mazenauer <umazenauer@hotmail.com>
2023-05-10 21:28:46 +0200

    prevent blocking width and made grids flexible

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,7 @@ export default function Hero() {
         <section className={styles.sectionHero}>
             <div className={styles.hero}>
                 <div className="hero-text-box">
-                    <h1 className="heading-primary">Einsatzplanung Samichlaus</h1>
+                    <h1 className="heading-primary">Einsatz&shy;planung Samichlaus</h1>
                     <p className={styles.heroDescription}>Die St.Nikolaus Gesellschaft Dietlikon &amp; Br&uuml;ttisellen plant und koordiniert die Eins&auml;tze seiner Mitglieder anhand derer Verf&uuml;gbarkeiten. Denn Eins&auml;tze wollen koordiniert und gut geplant sein.</p>
                     <p className={styles.heroDescription}>Sei Teil der Freiwilligen und unterst&uuml;tze uns!</p>
                     <Link href='/account/login' className="btn btn--full margin-right-sm">
