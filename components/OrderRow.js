9ec8946789282d9e1d647fc9d3c988371991cecb
Urs Mazenauer <umazenauer@hotmail.com>
2023-05-13 19:40:33 +0200

    responsive orders

--- components/OrderRow.js
+++ components/OrderRow.js
@@ -3,34 +3,38 @@ import Link from "next/link";
 import {IconEdit, IconTrash} from '@tabler/icons-react';
 import styles from '@/styles/OrderRow.module.scss';
 
-export default function OrderRow({ order, onDelete }) {
+export default function OrderRow({order, onDelete}) {
     const attributes = order.attributes;
 
     return (
         <div className={`${styles.order} ${styles[attributes.status]}`}>
-            <div className={styles.info}>
-                <div className={styles.infoTop}>
-                    <div>
-                        <strong>{formatDate(attributes.datum)}</strong>&nbsp;
-                        {formatTime(attributes.zeitVon)}
-                        {attributes.zeitVon && ' - '}
-                        {formatTime(attributes.zeitBis)}
+            <div className={styles.main}>
+                <div className={styles.info}>
+                    <div className={styles.infoTop}>
+                        <div>
+                            <strong>{formatDate(attributes.datum)}</strong>&nbsp;
+                            {formatTime(attributes.zeitVon)}
+                            {attributes.zeitVon && ' - '}
+                            {formatTime(attributes.zeitBis)}
+                        </div>
+                        <div>{attributes.bezeichnung} | <span
+                            className={`${styles[`${attributes.status}-bottom`]}`}>{attributes.status}</span></div>
                     </div>
-                    <div>{attributes.bezeichnung} | <span className={`${styles[`${attributes.status}-bottom`]}`}>{attributes.status}</span></div>
+                    <div className={styles.details}>
+                        <div className={styles.einsatztyp}>{attributes.einsatztyp?.typ}</div>
+                        <div>{attributes.adresse}</div>
+                    </div>
                 </div>
-                <div className={styles.details}>
-                    <div className={styles.einsatztyp}>{attributes.einsatztyp?.typ}</div>
-                    <div>{attributes.adresse}</div>
-                </div>
-            </div>
 
-            <div className={styles.link}>
-                <Link href={`/orders/edit/${order.id}`} className="btn btn-secondary btn-secondary--small btn-icon">
-                    <IconEdit />
-                </Link>
-                {!order.activity?.data && <button className="btn btn-secondary btn-secondary--small btn-icon" onClick={onDelete}>
-                    <IconTrash/>
-                </button>}
+                <div className={styles.link}>
+                    <Link href={`/orders/edit/${order.id}`} className="btn btn-secondary btn-secondary--small btn-icon">
+                        <IconEdit/>
+                    </Link>
+                    {!order.activity?.data &&
+                        <button className="btn btn-secondary btn-secondary--small btn-icon" onClick={onDelete}>
+                            <IconTrash/>
+                        </button>}
+                </div>
             </div>
         </div>
     )
