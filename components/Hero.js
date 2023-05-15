158d58de06cfccd44d185cdc111f22df9bf1975d
Urs Mazenauer <umazenauer@hotmail.com>
2023-05-15 21:24:56 +0200

    changed text in hero section

--- components/Hero.js
+++ components/Hero.js
@@ -11,7 +11,7 @@ export default function Hero() {
                 <div className="hero-text-box">
                     <h1 className="heading-primary">Einsatz&shy;planung Samichlaus</h1>
                     <p className={styles.heroDescription}>Die St.Nikolaus Gesellschaft Dietlikon &amp; Br&uuml;ttisellen plant und koordiniert die Eins&auml;tze seiner Mitglieder anhand derer Verf&uuml;gbarkeiten. Denn Eins&auml;tze wollen koordiniert und gut geplant sein.</p>
-                    <p className={styles.heroDescription}>Sei Teil der Freiwilligen und unterst&uuml;tze uns!</p>
+                    <p className={styles.heroDescription}>Unterst&uuml;tze den Schmutzli und melde dich an!</p>
                     <Link href='/account/login' className="btn btn--full margin-right-sm">
                         Login
                     </Link>
